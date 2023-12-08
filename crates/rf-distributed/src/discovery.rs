7af6f6549e1315935c03faccd4668f6576506d2a
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-12-08 11:09:11 +0100

    chore(docs): add comments for discovery

--- crates/rf-distributed/src/discovery.rs
+++ crates/rf-distributed/src/discovery.rs
@@ -1,3 +1,8 @@
+/// This trait represents a discovery strategy for the platform
 pub trait Discovery {
+    /// Discovers the neighbours of the device
+    ///
+    /// # Returns
+    /// A vector containing the ids of the neighbours
     fn discover_neighbors(&self) -> Vec<i32>;
 }
