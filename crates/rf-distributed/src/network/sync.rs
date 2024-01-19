a5c4a63cca3397196f1198a7d2b9b5d7b289d200
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-01-19 15:11:36 +0100

    refactor: change self to be mut in send

--- crates/rf-distributed/src/network/sync.rs
+++ crates/rf-distributed/src/network/sync.rs
@@ -1,6 +1,6 @@
 use crate::network::{NetworkResult, NetworkUpdate};
 
 pub trait Network {
-    fn send(&self, source: i32, msg: String) -> NetworkResult<()>;
+    fn send(&mut self, source: i32, msg: String) -> NetworkResult<()>;
     fn receive(&mut self) -> NetworkResult<NetworkUpdate>;
 }
