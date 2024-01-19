5971bd2e7bcfc1ebe492c7769bbc14ac954f5fea
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-01-19 12:31:40 +0100

    fix: fix imports

--- crates/rf-distributed-impl/src/network.rs
+++ crates/rf-distributed-impl/src/network.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 use bytes::Bytes;
-use rf_distributed::network::{Network, NetworkResult, NetworkUpdate};
+use rf_distributed::network::{asynchronous::Network, NetworkResult, NetworkUpdate};
 use rumqttc::{AsyncClient, MqttOptions, QoS};
 use tokio::sync::mpsc::Receiver;
 
