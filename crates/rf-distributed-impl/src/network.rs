791951283b5365b63bf6a8b362be696499175af8
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-02-12 14:44:53 +0100

    refactor: use bytes instead of string for network msg

--- crates/rf-distributed-impl/src/network.rs
+++ crates/rf-distributed-impl/src/network.rs
@@ -1,8 +1,10 @@
+use std::error::Error;
 use async_trait::async_trait;
 use bytes::Bytes;
 use rf_distributed::network::{asynchronous::Network, NetworkResult, NetworkUpdate};
 use rumqttc::{AsyncClient, MqttOptions, QoS};
 use tokio::sync::mpsc::Receiver;
+use log::info;
 
 /// This struct represent the network that will be used to send and receive messages
 /// using the MQTT protocol.
@@ -12,35 +14,46 @@ pub struct AsyncMQTTNetwork {
 }
 
 impl AsyncMQTTNetwork {
-    pub async fn new(options: MqttOptions, topics: Vec<i32>) -> Self {
-        let (client, mut eventloop) = AsyncClient::new(options, 10);
+    pub async fn new(options: MqttOptions, topics: Vec<i32>, mqtt_channel_cap: usize, network_buffer_size: usize) -> Result<Self, Box<dyn Error>> {
+        let (client, mut eventloop) = AsyncClient::new(options, mqtt_channel_cap);
         AsyncMQTTNetwork::subscribe_to_topics(client.clone(), topics)
-            .await
-            .unwrap();
-        let (sender, receiver) = tokio::sync::mpsc::channel::<NetworkUpdate>(100);
+            .await?;
+        let (sender, receiver) = tokio::sync::mpsc::channel::<NetworkUpdate>(network_buffer_size);
         tokio::spawn(async move {
             loop {
-                if let Ok(notification) = eventloop.poll().await {
-                    if let rumqttc::Event::Incoming(rumqttc::Packet::Publish(msg)) = notification {
-                        let msg_string = String::from_utf8(msg.payload.to_vec()).unwrap();
-                        sender
-                            .send(NetworkUpdate::Update { msg: msg_string })
-                            .await
-                            .unwrap();
+                match eventloop.poll().await {
+                    Ok(notification) => {
+                        match notification {
+                            rumqttc::Event::Incoming(rumqttc::Packet::Publish(msg)) => {
+                                if let Err(send_error) = sender
+                                    .send(NetworkUpdate::Update { msg: msg.payload })
+                                    .await {
+                                    info!("Error sending message to receiver: {:?}", send_error.to_string());
+                                }
+                            }
+                            _ => {
+
+                            }
+                        }
                     }
-                } else {
-                    sender.send(NetworkUpdate::None).await.unwrap();
+                    Err(e) => {
+                        if let Err(e2) = sender.send(NetworkUpdate::Err { reason: e.to_string() }).await {
+                            info!("Error: {:?}", e2.to_string());
+                        }
+                    }
                 }
             }
         });
-        Self { client, receiver }
+        Ok(Self { client, receiver })
     }
 
     async fn subscribe_to_topics(client: AsyncClient, topics: Vec<i32>) -> NetworkResult<()> {
         for nbr in topics.clone() {
-            client
+            if let Err(e) = client
                 .subscribe(format!("hello-rufi/{nbr}/subscriptions"), QoS::AtMostOnce)
-                .await?;
+                .await {
+                return Err(e.into());
+            }
         }
         Ok(())
     }
@@ -48,13 +61,13 @@ impl AsyncMQTTNetwork {
 
 #[async_trait]
 impl Network for AsyncMQTTNetwork {
-    async fn send(&mut self, source: i32, msg: String) -> NetworkResult<()> {
+    async fn send(&mut self, source: i32, msg: Bytes) -> NetworkResult<()> {
         self.client
             .publish(
                 format!("hello-rufi/{source}/subscriptions"),
                 QoS::AtMostOnce,
                 false,
-                Bytes::from(msg),
+                msg,
             )
             .await
             .map_err(|e| e.into())
