7bc0fb9edc42754f6cd928a045b4259280f6fcc0
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-02-19 10:11:59 +0100

    refactor: move single cycle inside struct impl

--- crates/rf-distributed/src/platform/sync.rs
+++ crates/rf-distributed/src/platform/sync.rs
@@ -73,14 +73,7 @@ impl<M, N, D, S, T, H> RuFiPlatform<M, N, D, S, T, H>
             A: Clone + 'static + FromStr + Display,
     {
         loop {
-            self.pre_cycle();
-            let export = single_cycle(
-                &mut self.mailbox,
-                &mut self.network,
-                &self.nbr_sensor_setup,
-                self.context.clone(),
-                program,
-            )?;
+            let export = self.single_cycle(program)?;
 
             for hook in self.hooks.iter() {
                 hook(&export);
@@ -96,21 +89,39 @@ impl<M, N, D, S, T, H> RuFiPlatform<M, N, D, S, T, H>
             A: Clone + 'static + FromStr + Display,
     {
         for _ in 0..n {
-            self.pre_cycle();
-
-            single_cycle(
-                &mut self.mailbox,
-                &mut self.network,
-                &self.nbr_sensor_setup,
-                self.context.clone(),
-                program,
-            )?;
+            self.single_cycle(program)?;
         }
         Ok(())
     }
 
-    /// Performs the pre-cycle operations
-    fn pre_cycle(&mut self) {
+    /// Performs a single step of the execution cycle of an aggregate program
+    ///
+    /// # Arguments
+    ///
+    /// * `mailbox` - The mailbox of the device
+    /// * `network` - The network through which the device communicates
+    /// * `context` - The context of the device
+    /// * `program` - The aggregate program to be executed
+    ///
+    /// # Generic Arguments
+    ///
+    /// * `P` - The type of the aggregate program, it must be a function that takes a [RoundVM] and returns a [RoundVM] and a result of type `A`
+    /// * `A` - The type of the result of the aggregate program
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), Box<dyn Error>>` - The result of the execution
+    fn single_cycle<P, A>(
+        &mut self,
+        program: P,
+    ) -> Result<Export, Box<dyn Error>>
+        where
+            P: Fn(&mut RoundVM) -> A,
+            A: Clone + 'static + FromStr + Display,
+            M: Mailbox,
+            N: Network,
+            S: NbrSensorSetup,
+    {
         // STEP 1: Discover neighbours
         let nbrs = self.discovery.discover_neighbors();
         // STEP 2: Subscribe to the topics of the neighbours
@@ -120,89 +131,58 @@ impl<M, N, D, S, T, H> RuFiPlatform<M, N, D, S, T, H>
             .filter(|n| !self.discovered_nbrs.contains(n))
             .collect();
         self.discovered_nbrs.extend(subscriptions);
-    }
-}
 
-/// Performs a single step of the execution cycle of an aggregate program
-///
-/// # Arguments
-///
-/// * `mailbox` - The mailbox of the device
-/// * `network` - The network through which the device communicates
-/// * `context` - The context of the device
-/// * `program` - The aggregate program to be executed
-///
-/// # Generic Arguments
-///
-/// * `P` - The type of the aggregate program, it must be a function that takes a [RoundVM] and returns a [RoundVM] and a result of type `A`
-/// * `A` - The type of the result of the aggregate program
-///
-/// # Returns
-///
-/// * `Result<(), Box<dyn Error>>` - The result of the execution
-fn single_cycle<P, A, M, N, S>(
-    mailbox: &mut M,
-    network: &mut N,
-    setup: &S,
-    context: Context,
-    program: P,
-) -> Result<Export, Box<dyn Error>>
-    where
-        P: Fn(&mut RoundVM) -> A,
-        A: Clone + 'static + FromStr + Display,
-        M: Mailbox,
-        N: Network,
-        S: NbrSensorSetup,
-{
-    //STEP 3: Retrieve the neighbouring exports from the mailbox
-    let states = mailbox.messages().as_states();
+        //STEP 3: Retrieve the neighbouring exports from the mailbox
+        let states = self.mailbox.messages().as_states();
 
-    //STEP 4: Execute a round
-    let nbr_sensors = setup.nbr_sensor_setup(states.keys().cloned().collect());
-    let context = Context::new(
-        *context.self_id(),
-        context.local_sensors().clone(),
-        nbr_sensors,
-        states,
-    );
-    //println!("CONTEXT: {:?}", context);
-    let mut vm = RoundVM::new(context);
-    vm.new_export_stack();
-    let result = round(&mut vm, program);
-    let self_export: Export = vm.export_data().clone();
-    println!("OUTPUT: {}\nEXPORT: {}\n", result, self_export);
+        //STEP 4: Execute a round
+        let nbr_sensors = self.nbr_sensor_setup.nbr_sensor_setup(states.keys().cloned().collect());
+        let context = Context::new(
+            *self.context.self_id(),
+            self.context.local_sensors().clone(),
+            nbr_sensors,
+            states,
+        );
+        //println!("CONTEXT: {:?}", context);
+        let mut vm = RoundVM::new(context);
+        vm.new_export_stack();
+        let result = round(&mut vm, program);
+        let self_export: Export = vm.export_data().clone();
+        println!("OUTPUT: {}\nEXPORT: {}\n", result, self_export);
 
-    //STEP 5: Publish the export
-    let msg = Message::new(*vm.self_id(), self_export.clone(), std::time::SystemTime::now());
-    if let Ok(msg_ser) = serde_json::to_vec(&msg) {
-        if let Err(e) = network.send(*vm.self_id(), Bytes::from(msg_ser)) {
-            println!("Error sending the message: {}", e);
+        //STEP 5: Publish the export
+        let msg = Message::new(*vm.self_id(), self_export.clone(), std::time::SystemTime::now());
+        if let Ok(msg_ser) = serde_json::to_vec(&msg) {
+            if let Err(e) = self.network.send(*vm.self_id(), Bytes::from(msg_ser)) {
+                println!("Error sending the message: {}", e);
+            }
+        } else {
+            println!("Error while serializing the message");
         }
-    } else {
-        println!("Error while serializing the message");
-    }
 
-    //STEP 6: Receive the neighbouring exports from the network
-    match network.receive() {
-        Ok(NetworkUpdate::Update { msg }) => {
-            if let Ok(msg) = serde_json::from_slice(&msg) {
-                mailbox.enqueue(msg);
+        //STEP 6: Receive the neighbouring exports from the network
+        match self.network.receive() {
+            Ok(NetworkUpdate::Update { msg }) => {
+                if let Ok(msg) = serde_json::from_slice(&msg) {
+                    self.mailbox.enqueue(msg);
+                    Ok(self_export)
+                } else {
+                    Err("Error deserializing the message".into())
+                }
+            }
+            Ok(NetworkUpdate::None) => {
+                println!("No message received from the network");
                 Ok(self_export)
-            } else {
-                Err("Error deserializing the message".into())
+            }
+            Ok(NetworkUpdate::Err { reason }) => {
+                println!("Error receiving from the network: {}", reason);
+                Err(reason.into())
+            }
+            _ => {
+                println!("Error receiving from the network");
+                Err("Error receiving from the network".into())
             }
         }
-        Ok(NetworkUpdate::None) => {
-            println!("No message received from the network");
-            Ok(self_export)
-        }
-        Ok(NetworkUpdate::Err { reason }) => {
-            println!("Error receiving from the network: {}", reason);
-            Err(reason.into())
-        }
-        _ => {
-            println!("Error receiving from the network");
-            Err("Error receiving from the network".into())
-        }
     }
 }
+
